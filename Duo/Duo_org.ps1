c0414462fac9c1bc88af4fd6988e0e7319c3bd8e
John Cummings <jcummi21@ITS0WHG8WLALT.uncc.edu>
2017-07-21 09:12:20 -0400

    Commit me

---
+++ Duo/Duo_org.ps1
@@ -0,0 +1,16 @@
+﻿<# Duo_org.ps1 #>
+# define the default Duo Org/Instance you want to use, useful if you have more than one.
+[string]$DuoDefaultOrg = "prod"
+
+[Hashtable]$DuoOrgs = @{
+                        prod = [Hashtable]@{
+                                iKey  = [string]"DIN73Q13I1E2Z5CJ7NUU"
+                                sKey = [string]"3oILJHZJDSGRPy5vMmctMzpSv2oyPnIFPKyNObTz"
+                                apiHost = [string]"api-c9cc3e0e.duosecurity.com"
+                               }
+                        etst = [Hashtable]@{
+                                iKey  = [string]"DIxxxxxxxxxxxxxxxxxx"
+                                sKeyEnc = [string]"Big Long protected string on 1 line here"
+                                apiHost = [string]"api-nnnnnxnx.duosecurity.com"
+							   }
+                       }
--- README.md
+++
@@ -1,3 +0,0 @@
-# DeleteAllUsersInOU-Powershell
-
-A small PS script allowing you to specify an OU by distinquishedName and loop through all users objects in that OU permanently deleting them
